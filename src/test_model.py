e9ae2c9a83f025148e7fc6bea0eedf34602704b9
hooshmandshr <shokri.hooshmand@gmail.com>
2018-11-28 16:15:57 -0500

    Fixed a test for model. Scale parameter of distribution no longer is passed.

--- src/test_model.py
+++ src/test_model.py
@@ -29,27 +29,20 @@ def test_reparam_gaussian_full_covar():
                 out_dim=out_dim, in_dim=in_dim,
                 distribution=dist, transform=LinearTransform,
                 reparam_scale=False)
-        d_3 = ReparameterizedDistribution(
-                out_dim=out_dim, in_dim=in_dim,
-                distribution=dist, transform=LinearTransform,
-                reparam_scale=tf.eye(out_dim, dtype=input_tensor.dtype))
         s_1 = d_1.sample(n_samples=n_sample, y=input_tensor)
         s_2 = d_2.sample(n_samples=n_sample, y=input_tensor)
-        s_3 = d_3.sample(n_samples=n_sample, y=input_tensor)
 
         cov_2 = d_2.get_distribution(y=input_tensor).covariance()
-        cov_3 = d_3.get_distribution(y=input_tensor).covariance()
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            tf_res = sess.run([s_1, s_2, s_3])
-            tf_cov = sess.run([cov_2, cov_3])
+            tf_res = sess.run([s_1, s_2])
+            tf_cov = sess.run([cov_2])
 
     expected_shape = (n_sample, tot_dist, out_dim)
     print "Testing the shape of the samples."
-    for i in range(3):
+    for i in range(2):
         assert tf_res[i].shape == expected_shape, "sample shape not correct."
     print "Testing that the covariance of the distributions are all the same."
-    assert np.allclose(np.array(np.eye(2)), tf_cov[1])
     assert np.allclose(tf_cov[0][0], tf_cov[0][1])
 
 if __name__ == "__main__":
