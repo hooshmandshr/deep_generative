Fixed bug with initialization of MLP and linear transformation. Added initialization for Normalizing flows.

src/transform.py
@@ -135,8 +135,10 @@ class LinearTransform(Transform):
 
     def initializer(self):
         """Overriden function to do Xavier initialization."""
-        self.var = tf.Variable(np.random.normal(
-                0, np.sqrt(1. / self.in_dim), self.param_shape))
+        init_val = np.random.normal(
+                0, np.sqrt(2. / self.in_dim + self.out_dim), self.param_shape)
+        init_val[-1] *= 0.
+        self.var = tf.Variable(init_val)
 
     def get_regularizer(self, scale=1.):
         return scale * tf.reduce_sum(tf.reduce_sum(tf.square(self.lin_trans)))
@@ -292,7 +294,7 @@ class LSTMcell(Transform):
     def initializer(self):
         """Overriden function to do Xavier initialization."""
         self.var = tf.Variable(np.random.normal(
-                0, np.sqrt(1. / self.in_dim), self.param_shape))
+                0, np.sqrt(2. / self.in_dim + self.out_dim), self.param_shape))
 
     def get_regularizer(self, scale=1.):
         return NotImplementedError("LSTM does not have.")