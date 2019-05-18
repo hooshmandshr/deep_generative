Bug Fixe: dynamics computation of log_prp and added tests for it. New: dropout. Reparameterized distribution parameters share networks.

src/aevb.py
@@ -17,7 +17,7 @@ class AutoEncodingVariationalBayes(object):
 
     def __init__(self, data, generative_model, recognition_model, prior=None,
             optimizer=None, n_monte_carlo_samples=1, batch_size=1,
-            reg_coeff=0.01):
+            drop_out=None, reg_coeff=0.01):
         """
         params:
         -------
@@ -94,10 +94,11 @@ class AutoEncodingVariationalBayes(object):
                 mc_samples_log_prob = self.rec_model.log_prob(
                         x=mc_samples, y=self.batch)
 
-            entropy = -tf.reduce_mean(mc_samples_log_prob, axis=-1)
+            entropy = - tf.reduce_mean(mc_samples_log_prob, axis=0)
+
         self.entropy = entropy
 
-        self.elbo = tf.reduce_mean(self.expected_likelihood + self.entropy)
+        self.elbo = tf.reduce_mean(self.expected_likelihood + self.entropy, axis=0)
         # Reconstruction of data
         self.codes = mc_samples
         self.recon = self.gen_model.sample(n_samples=self.sample_size, y=mc_samples)

src/dynamics.py
@@ -62,12 +62,21 @@ class MarkovDynamics(Model):
             (?, T, D)
         """
         n_sample = x.shape[0].value
-        log_prob = self.init_model.log_prob(
-                tf.reshape(x[:, :self.order, :], [n_sample, -1]))
-        for i in range(self.order, self.time_steps):
-            y = tf.reshape(x[:, i - self.order:i, :], [n_sample, -1])
-            log_prob += self.transition_model.log_prob(
-                    x=x[:, i, :], y=y)
+        x_init = [x[..., i, :] for i in range(self.order)]
+        x_pre = [x[..., (0 + i):(-self.order + i), :] for i in range(self.order)]
+        if self.order == 1:
+            x_init = x_init[0]
+            x_pre = x_pre[0]
+        else:
+            x_init = tf.concat(x_init, axis=-1)
+            x_pre = tf.concat(x_init, axis=-1)
+
+        log_prob = self.init_model.log_prob(x_init)
+        log_prob += tf.reduce_sum(
+                self.transition_model.log_prob(
+                    x=x[..., self.order:, :],
+                    y=x_pre),
+                axis=-1)
         return log_prob
 
     def sample(self, n_samples, init_states=None, time_steps=None,
@@ -157,7 +166,8 @@ class TimeVariantDynamics(Model):
                         transform=MLP,
                         distribution=self.dist_type,
                         hidden_units=trans_hidden_units))
-        super(TimeVariantDynamics, self).__init__(out_dim=self.state_dim * time_steps)
+        super(TimeVariantDynamics, self).__init__(
+                out_dim=self.state_dim * time_steps)
 
     def sample(self, n_samples):
         """Log probability of the dynamics model p(x) = p(x1, x2, ...).
@@ -188,6 +198,7 @@ class TimeVariantDynamics(Model):
         for t, model in enumerate(self.time_transition_model):
             log_prob += model.log_prob(x=x[:, t + 1], y=x[:, t])
         return log_prob
+
 
 class MarkovLatentDynamics(MarkovDynamics):
     """Class for expressing Laten Markov dynamics."""
@@ -237,23 +248,23 @@ class MarkovLatentDynamics(MarkovDynamics):
         tf.Tensor with shape (?), corresponding to the log-probability of
         joint-distribution given the input.
         """
-        out_shape = None
-        if not x.shape[:-2] == y.shape[:-2]:
-            if len(x.shape) + 1 == len(y.shape):
-                out_shape = y.shape[0].value
-                x = tf.reshape(tf.concat(
-                        [tf.expand_dims(x, axis=0) for i in range(out_shape)],
-                        axis=0), [-1] + x.shape[-2:].as_list())
-                y = tf.reshape(y, [-1] + y.shape[-2:].as_list())
-            else:
-                raise ValueError("Shape of inputs x, y does not match.")
+        log_p = None
+        if not(x.shape[-3:-1] == y.shape[-3:-1]):
+            raise ValueError("Shape of inputs x, y does not match.")
+        if len(x.shape) == 3 and len(y.shape) == 3:
+            log_p = tf.reduce_sum(self.emission_model.log_prob(
+                x=x, y=y), axis=-1)
+        elif len(x.shape) == 3 and len(y.shape) == 4:
+            log_p = []
+            for i in range(x.shape[0].value):
+                log_p.append(tf.reduce_sum(self.emission_model.log_prob(
+                    x=x[i:i+1], y=y[:, i:i+1]), axis=-1))
+            log_p = tf.concat(log_p, axis=1)
+        else:
+            raise ValueError("Shape of inputs x, y does not match.")
 
-        prior_log_prob = super(MarkovLatentDynamics, self).log_prob(x=y)
-        log_p = tf.reduce_sum(self.emission_model.log_prob(
-                x=x, y=y)) + prior_log_prob
-        if out_shape is not None:
-            return tf.reshape(log_p, [out_shape, -1])
-        return log_p
+        # Add prior
+        return log_p + super(MarkovLatentDynamics, self).log_prob(x=y)
 
     def sample(self, n_samples, init_states=None, y=None, time_steps=None,
             stochastic=True):

src/model.py
@@ -170,7 +170,7 @@ class ReparameterizedDistribution(Model):
         if self.dist_class is MultiplicativeNormal: 
             in_time, in_dim = self.in_dim
             time, out_dim = self.out_dim
-            # Parameters of the C matrix and M matrix.
+            # Parameters of the C matrix and M matrix share network.
             self.transforms.append(self.transform_class(
                 in_dim=in_dim,
                 out_dim=out_dim * out_dim + out_dim,
@@ -181,24 +181,26 @@ class ReparameterizedDistribution(Model):
                 in_dim=self.in_dim[1],
                 out_dim=self.out_dim[1] * 2 + self.extra_dim,
                 **self.trans_args))
-        else: 
-            self.transforms.append(self.transform_class(
-                in_dim=self.in_dim, out_dim=self.out_dim,
-                **self.trans_args))
-
-        # Reparameteriz scale if needed.
-        if self.reparam_scale == True:
-            if self.dist_class is DIAG_GAUSSIAN or\
-                    self.dist_class is LogitNormalDiag:
-                # Diagonal Covariance.
+        else:
+            # Other distributions.
+            if self.reparam_scale is not True:
+                # Scale parameter of the distribution is free or constant.
                 self.transforms.append(self.transform_class(
                     in_dim=self.in_dim, out_dim=self.out_dim,
                     **self.trans_args))
-            # Multivariate Normal With full covariance.
-            elif self.dist_class is FULL_GAUSSIAN:
-                # Cholesky factor of the covariance matrix.
+            else:
+                # main parameter and scale of distribution share network.
+                if self.dist_class is DIAG_GAUSSIAN or\
+                        self.dist_class is LogitNormalDiag:
+                    # Mean + Diagonal Covariance.
+                    trans_out_dim = 2 * self.out_dim
+                # Multivariate Normal With full covariance.
+                elif self.dist_class is FULL_GAUSSIAN:
+                    # Mean + Cholesky factor of the covariance matrix.
+                    trans_out_dim = self.out_dim + self.out_dim * self.out_dim
                 self.transforms.append(self.transform_class(
-                    in_dim=self.in_dim, out_dim=self.out_dim * self.out_dim,
+                    in_dim=self.in_dim,
+                    out_dim=trans_out_dim,
                     **self.trans_args))
 
         return self.transforms
@@ -239,28 +241,21 @@ class ReparameterizedDistribution(Model):
                 return dist
             # Rectify standard deviation so that it is a smooth
             # positive function
-            loc_ = transforms[0].operator(y)
-            scale_ = self.reparam_scale
+            out_ = transforms[0].operator(y)
+            loc_ = out_
+            scale_ = self.scale_param
             if self.reparam_scale is True:
-                scale_ = tf.nn.softplus(transforms[1].operator(y))
-            else:
-                if self.reparam_scale is False:
-                    if self.scale_param is None:
-                        scale_ = tf.Variable(
-                                np.zeros(self.out_dim), dtype=loc_.dtype)
-                        self.scale_param = scale_
-                    else:
-                        scale_ = self.scale_param
-                tot_unique_dist = 1
-                for dim in loc_.shape[:-1]:
-                    tot_unique_dist *= dim.value
-                scale_ = [tf.expand_dims(
-                    scale_, axis=0) for i in range(tot_unique_dist)]
-                scale_ = tf.concat(scale_, axis=0)
-                scale_shape = loc_.shape[:-1].as_list(
-                        ) + [self.out_dim]
-                scale_ = tf.nn.softplus(tf.reshape(scale_, scale_shape))
-
+                loc_, scale_ = out_[..., :self.out_dim], out_[..., self.out_dim:]
+            elif scale_ is None and self.reparam_scale is False:
+                # Initialize a free scale param.
+                scale_ = tf.Variable(
+                        np.zeros(self.out_dim), dtype=loc_.dtype)
+                self.scale_param = scale_
+            elif self.scale_param is None:
+                # standard devition is given.
+                scale_ = self.reparam_scale
+            # Ensure that std is positive.
+            scale_ = tf.nn.softplus(scale_)
             dist = self.dist_class(loc=loc_, scale_diag=scale_)
 
         elif self.dist_class is StateSpaceNormalDiag:
@@ -292,30 +287,22 @@ class ReparameterizedDistribution(Model):
 
         # Multivariate Normal With full covariance.
         elif self.dist_class is FULL_GAUSSIAN:
-            loc_ = transforms[0].operator(y)
-            cov_ = self.reparam_scale
+            out_ = transforms[0].operator(y)
+            loc_ = out_
+            cov_ = self.scale_param
             if self.reparam_scale is True:
-                cov_ = transforms[1].operator(y)
-                cov_ = tf.reshape(cov_, [-1, self.out_dim, self.out_dim])
-            else:
-                if self.reparam_scale is False:
-                    if self.scale_param is None:
-                        cov_ = tf.Variable(
-                                np.eye(self.out_dim), dtype=loc_.dtype)
-                        self.scale_param = cov_
-                    else:
-                        cov_ = self.scale_param
-                # The covariance should have the same shape as the mean
-                tot_unique_dist = 1
-                for dim in loc_.shape[:-1]:
-                    tot_unique_dist *= dim.value
-                cov_ = [tf.expand_dims(
-                    cov_, axis=0) for i in range(tot_unique_dist)]
-                cov_ = tf.concat(cov_, axis=0)
-                cov_shape = loc_.shape[:-1].as_list(
-                        ) + [self.out_dim, self.out_dim]
-                cov_ = tf.reshape(cov_, cov_shape)
-
+                loc_, cov_ = out_[..., :self.out_dim], out_[..., self.out_dim:]
+                cov_ = tf.reshape(
+                        cov_,
+                        cov_.shape.as_list()[:-1] + [self.out_dim, self.out_dim])
+            elif cov_ is None and self.reparam_scale is False:
+                # create a free parameter for covariance.
+                cov_ = tf.Variable(
+                        np.eye(self.out_dim), dtype=loc_.dtype)
+                self.scale_param = cov_
+            elif self.scale_param is None:
+                # Covariance is given.
+                cov_ = self.reparam_scale
             dist = self.dist_class(loc=loc_, scale_tril=cov_)
 
         # MultiplicativeNormal for LDS models.

src/test_dynamics.py
@@ -4,7 +4,8 @@
 import numpy as np
 import tensorflow as tf
 
-from dynamics import KalmanFilter, MarkovDynamicsDiagnostics
+from dynamics import KalmanFilter, MarkovDynamicsDiagnostics, MLPDynamics
+from transform import MultiLayerPerceptron
 
 
 def test_kalman_filter(full_covar=False):
@@ -70,6 +71,64 @@ def test_kalman_filter(full_covar=False):
         assert grid_out[0].shape == (grid_size, 2, lat_dim)
 
 
+def test_mlp_dynamics():
+    """Tester for MLPDynamics and MarkovLatentDynamics in general."""
+    n_ex, dim, dim_o, time, n_sample = 2, 3, 4, 5, 6
+    with tf.Graph().as_default():
+        model = MLPDynamics(
+            lat_dim=dim, obs_dim=dim_o, time_steps=time,
+            transition_layers=[4],
+            emission_transform=MultiLayerPerceptron,
+            full_covariance=False,
+            hidden_units=[5])
+
+        x = np.ones([n_ex, time, dim_o])
+        y = np.ones([n_sample, n_ex, time, dim])
+
+        x_ = tf.constant(x)
+        y_ = tf.constant(y)
+        p = model.log_prob(x_, y_)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            out = sess.run(p)
+    assert out.shape == (n_sample, n_ex)
+
+    with tf.Graph().as_default():
+        model = MLPDynamics(
+            lat_dim=dim, obs_dim=dim_o, time_steps=time,
+            transition_layers=[4],
+            emission_transform=MultiLayerPerceptron,
+            full_covariance=False,
+            hidden_units=[5])
+
+        x = np.ones([2, time, dim_o])
+        y = np.ones([2, time, dim])
+
+        x_ = tf.constant(x)
+        y_ = tf.constant(y)
+        p = model.log_prob(x_, y_)
+
+        x[1, 1] += 10
+        x_ = tf.constant(x)
+        p2 = model.log_prob(x_, y_)
+
+        y[0, 1] += 10
+        y_ = tf.constant(y)
+        p3 = model.log_prob(x_, y_)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            out1 = sess.run(p)
+            out2 = sess.run(p2)
+            out3 = sess.run(p3)
+
+    assert out1[0] == out1[1]
+    assert out1[0] == out2[0]
+    assert not out2[0] == out2[1]
+    assert out2[1] == out3[1]
+    assert not out3[0] == out3[1]
+
 if __name__ == "__main__":
     test_kalman_filter(full_covar=False)
     test_kalman_filter(full_covar=True)
+    test_mlp_dynamics()

src/test_model.py
@@ -9,6 +9,44 @@ from model import ReparameterizedDistribution
 from transform import LinearTransform, MultiLayerPerceptron
 
 
+def test_reparam_gaussian_diag_covar():
+    """Tester for correctness of linear transformation."""
+
+    in_dim = 3
+    out_dim = 2
+    n_sample = 1000
+
+    tot_dist = 2
+    input_ = np.random.rand(tot_dist, in_dim)
+    dist = tf.contrib.distributions.MultivariateNormalDiag
+
+    with tf.Graph().as_default():
+        input_tensor = tf.constant(input_)
+        d_1 = ReparameterizedDistribution(
+                out_dim=out_dim, in_dim=in_dim,
+                distribution=dist, transform=LinearTransform,
+                reparam_scale=True)
+        d_2 = ReparameterizedDistribution(
+                out_dim=out_dim, in_dim=in_dim,
+                distribution=dist, transform=LinearTransform,
+                reparam_scale=False)
+        s_1 = d_1.sample(n_samples=n_sample, y=input_tensor)
+        # Repeat for testing sampling multiple times.
+        s_1 = d_1.sample(n_samples=n_sample, y=input_tensor)
+        s_2 = d_2.sample(n_samples=n_sample, y=input_tensor)
+        s_2 = d_2.sample(n_samples=n_sample, y=input_tensor)
+
+        cov_2 = d_2.get_distribution(y=input_tensor).covariance()
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            tf_res = sess.run([s_1, s_2])
+
+    expected_shape = (n_sample, tot_dist, out_dim)
+    print("Testing the shape of the samples.")
+    for i in range(2):
+        assert tf_res[i].shape == expected_shape, "sample shape not correct."
+
+
 def test_reparam_gaussian_full_covar():
     """Tester for correctness of linear transformation."""
 
@@ -31,6 +69,9 @@ def test_reparam_gaussian_full_covar():
                 distribution=dist, transform=LinearTransform,
                 reparam_scale=False)
         s_1 = d_1.sample(n_samples=n_sample, y=input_tensor)
+        # Repeat for testing sampling multiple times.
+        s_1 = d_1.sample(n_samples=n_sample, y=input_tensor)
+        s_2 = d_2.sample(n_samples=n_sample, y=input_tensor)
         s_2 = d_2.sample(n_samples=n_sample, y=input_tensor)
 
         cov_2 = d_2.get_distribution(y=input_tensor).covariance()
@@ -43,8 +84,6 @@ def test_reparam_gaussian_full_covar():
     print("Testing the shape of the samples.")
     for i in range(2):
         assert tf_res[i].shape == expected_shape, "sample shape not correct."
-    print("Testing that the covariance of the distributions are all the same.")
-    assert np.allclose(tf_cov[0][0], tf_cov[0][1])
 
 
 def test_state_space_normal_diag():
@@ -76,5 +115,6 @@ def test_state_space_normal_diag():
 
 
 if __name__ == "__main__":
+    test_reparam_gaussian_diag_covar()
     test_reparam_gaussian_full_covar()
     test_state_space_normal_diag()

src/transform.py
@@ -51,8 +51,10 @@ class Transform(object):
         """
         if self.var is not None:
             if not self.var.shape == self.param_shape:
-                raise ValueError("gov_param tensor's shape must be {}".format(
-                    self.param_shape))
+                msg = "gov_param tensor's shape must be {}".format(
+                        self.param_shape)
+                msg += " instead it is {}".format(self.var.shape)
+                raise ValueError(msg)
         elif self.initial_value is None:
             self.initializer()
         else:
@@ -258,7 +260,7 @@ class MultiLayerPerceptron(Transform):
 
     def __init__(self, in_dim, out_dim, hidden_units,
             activation=tf.nn.relu, residual=False, output_activation=None,
-            name=None):
+            drop_out_rate=None, name=None):
         """
         Sets up the layers of the MLP transformation.
 
@@ -274,22 +276,29 @@ class MultiLayerPerceptron(Transform):
         super(MultiLayerPerceptron, self).__init__(
                 in_dim=in_dim, out_dim=out_dim, name=name)
 
+        if isinstance(activation, list):
+            if not len(activation) == len(hidden_units):
+                msg = "activation and hidden_units must be of same length."
+                raise ValueError(msg)
         self.activation = activation
         self.out_activation = output_activation
         self.hidden_units = hidden_units
         # List that will containt the individual transformation for each layer.
         self.layers = []
-        activation = self.activation
+        activ_ = self.activation
         for i, n_units in enumerate(hidden_units + [self.out_dim]):
             # Apply output layers non-linearity for if it is the last layer.
             if i == len(hidden_units):
-                activation = self.out_activation
+                activ_ = self.out_activation
+            elif isinstance(self.activation, list):
+                activ_ = self.activation[i]
             layer_t = LinearTransform(
                     in_dim=in_dim, out_dim=n_units,
-                    non_linearity=activation)
+                    non_linearity=activ_)
             self.layers.append(layer_t)
             in_dim = n_units
         self.residual = residual
+        self.drop_out_rate = drop_out_rate
 
     def get_regularizer(self, scale=1.):
         """Regularizer for the weights of the multilayer perceptron."""
@@ -315,8 +324,12 @@ class MultiLayerPerceptron(Transform):
         if len(self.hidden_units) == 0:
             return x[..., :self.out_dim]
         output = x
-        for layer in self.layers:
+        for i, layer in enumerate(self.layers):
             output = layer.operator(output)
+            if self.drop_out_rate is not None and i <= len(self.hidden_units):
+                # only for hidden_layers
+                keep_prob = 1 - self.drop_out_rate
+                output = tf.nn.dropout(output, keep_prob=keep_prob)
         if self.residual:
             return x + output
         return output